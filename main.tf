306db9a450aebd743c4ff06575ed1d57221cdaaa
Dasha <d.gazkaeva@gmail.com>
2025-02-08 19:23:45 +0300

    add cloud vm

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone                     = var.zone
+  service_account_key_file = pathexpand("~/.yc-keys/key.json")
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "Cloud id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Folder id"
+}
+
+variable "zone" {
+  type        = string
+  description = "Yandex.Cloud Zone"
+  default     = "ru-central1-d"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "web-server-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "web-server-subnet"
+  zone           = var.zone
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = yandex_vpc_network.network.id
+}
+
+# Получение  последеней версии публичного образа с Ubuntu 24.04
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2404-lts-oslogin"
+}
+
+resource "yandex_compute_disk" "boot_disk" {
+  name     = "web-server-boot-disk"
+  type     = "network-ssd"
+  image_id = data.yandex_compute_image.ubuntu.id
+  size     = 10
+}
+
+resource "yandex_compute_instance" "server" {
+  name        = "web-server"
+  platform_id = "standard-v3"
+  hostname    = "web"
+
+  resources {
+    core_fraction = 20
+    cores         = 2
+    memory        = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot_disk.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+output "web-server-ip" {
+  value = yandex_compute_instance.server.network_interface[0].nat_ip_address
+}
